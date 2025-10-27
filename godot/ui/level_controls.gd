fe3b1d2259883ac2cd6f99d5dbfbc5134e31c9fc
halleyre <me@halleyre.dev>
2025-10-27 14:05:47 +1100

    Issue #5 Level Control - Camera Movement added - pan/move toggle - camera panning with keys and buttons

--- godot/ui/level_controls.gd
+++ godot/ui/level_controls.gd
@@ -4,13 +4,17 @@ extends AspectRatioContainer
 signal move_just_pressed(direction)
 signal move_released(direction)
 const move_actions = {
-	Frog.UP: 	"ui_up",
+	Frog.UP:    "ui_up",
 	Frog.RIGHT: "ui_right",
-	Frog.DOWN:	"ui_down",
-	Frog.LEFT:	"ui_left"}
+	Frog.DOWN:  "ui_down",
+	Frog.LEFT:  "ui_left"}
 var move_buttons
 
-signal pan(direction)
+signal pan_just_pressed(direction)
+signal pan_released(direction)
+var pan_mode = false
+const pan_mode_action = "ui_select"
+
 signal focus(dir)
 const focus_actions = { # order is important here as ui-prev contains ui-next
 	Level.PREV_F: "ui_focus_prev",
@@ -20,10 +24,10 @@ func _ready():
 	get_viewport().size_changed.connect(restore_controls)
 	restore_controls()
 	move_buttons = {
-		$VBoxContainer/Up: 		Frog.UP,
-		$HBoxContainer/Right:	Frog.RIGHT,
-		$VBoxContainer/Down:	Frog.DOWN,
-		$HBoxContainer/Left:	Frog.LEFT}
+		$VBoxContainer/Up:    Frog.UP,
+		$HBoxContainer/Right: Frog.RIGHT,
+		$VBoxContainer/Down:  Frog.DOWN,
+		$HBoxContainer/Left:  Frog.LEFT}
 
 	
 # is there a better way to stop the controls from falling off the screen?
@@ -34,16 +38,32 @@ func restore_controls():
 func _input(event: InputEvent):
 	for dir in move_actions:
 		if event.is_action_pressed(move_actions[dir]):
-			move_just_pressed.emit(dir)
+			if pan_mode: pan_just_pressed.emit(dir)
+			else:        move_just_pressed.emit(dir)
 		if event.is_action_released(move_actions[dir]):
-			move_released.emit(dir)
-
+			if pan_mode: pan_released.emit(dir)
+			else:        move_released.emit(dir)
+	
+	if event.is_action_pressed(pan_mode_action): toggle_pan()
+	
 	for dir in focus_actions:
 		if event.is_action_pressed(focus_actions[dir]):
 			focus.emit(dir)
 			break
 
-func _on_nav(source: BaseButton) -> void:
+func toggle_pan():
+	pan_mode = !pan_mode
+	$VBoxContainer/Move.visible = !$VBoxContainer/Move.visible
+	$HBoxContainer/Pan.visible = !$HBoxContainer/Pan.visible
+
+func _on_nav(source):
 	for btn in move_buttons:
 		if source == btn:
-			move_just_pressed.emit(move_buttons[btn])
+			if pan_mode: pan_just_pressed.emit(move_buttons[btn])
+			else:        move_just_pressed.emit(move_buttons[btn])
+
+func _on_nav_release(source):
+	for btn in move_buttons:
+		if source == btn:
+			if pan_mode: pan_released.emit(move_buttons[btn])
+			else:        move_released.emit(move_buttons[btn])
