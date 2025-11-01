0a09b8d5ae73bab7c2e053ccbd18c18d2e9c948e
halleyre <me@halleyre.dev>
2025-11-01 22:41:59 +1100

    WIP #8 Control Adv - Tile Aware Movement added - raycast movement and collision boxes ready for detection

--- godot/frogs/frog.gd
+++ godot/frogs/frog.gd
@@ -1,8 +1,5 @@
 class_name Frog
-extends Sprite2D
-
-var map: TileMapLayer
-var pos_i: Vector2i
+extends Area2D
 
 func invalid_move():
 	pass
@@ -11,12 +8,13 @@ func move_back():
 	invalid_move()
 
 func _on_move_just_pressed(dir):
-	if roundi(Level.DIR_VECTORS[dir].dot(Vector2.from_angle(rotation))) == -1:
+	var dir_v = Level.DIR_VECTORS[dir]
+	if roundi(dir_v.dot(Vector2.from_angle(rotation))) == -1:
 		move_back()
 		return
 
-	rotation = Level.DIR_VECTORS[dir].angle()
-	position += Level.DIR_VECTORS[dir] * 32
+	rotation = dir_v.angle()
+	position = transform * get_node("RayCast2D").target_position
 	
 	
 	
--- godot/levels/level.gd
+++ godot/levels/level.gd
@@ -18,17 +18,17 @@ var active_pan = {}
 @export var camera_speed = 200
 
 func _ready():
-	# start on frog with camera
+	# init frogs
 	frogs = get_children().filter(func(node): return node is Frog)
-	active_frog = frogs.find_custom(func(node): return node.has_node("Focus"))
-	
-	# init camera
-	camera = frogs[active_frog].get_node("Focus")
-	for dir in DIR_VECTORS:
-		active_pan[dir] = false
+	for frog in frogs: if frog.has_node("Focus"): active_frog = frog
 	
 	# connect frog signals
-	controls.move_just_pressed.connect(frogs[active_frog]._on_move_just_pressed)
+	controls.move_just_pressed.connect(active_frog._on_move_just_pressed)
+	
+	# init camera
+	camera = active_frog.get_node("Focus")
+	for dir in DIR_VECTORS:
+		active_pan[dir] = false
 	
 	# connect level signals
 	controls.pan_just_pressed.connect(_on_pan_just_pressed)
@@ -48,15 +48,15 @@ func _on_pan_released(dir):
 	active_pan[dir] = false
 	
 func focus_new(new_frog):
-	controls.move_just_pressed.disconnect(frogs[active_frog]._on_move_just_pressed)
+	controls.move_just_pressed.disconnect(active_frog._on_move_just_pressed)
 	camera.position = Vector2.ZERO
 	camera.reparent(frogs[new_frog], false)
-	active_frog = new_frog
-	controls.move_just_pressed.connect(frogs[active_frog]._on_move_just_pressed)
+	active_frog = frogs[new_frog]
+	controls.move_just_pressed.connect(active_frog._on_move_just_pressed)
 
 enum {PREV_F, NEXT_F}
 func _on_focus(dir):
 	if dir == PREV_F:
-		focus_new(posmod(active_frog - 1, frogs.size()))
+		focus_new(posmod(frogs.find(active_frog) - 1, frogs.size()))
 	if dir == NEXT_F:
-		focus_new(posmod(active_frog + 1, frogs.size()))
+		focus_new(posmod(frogs.find(active_frog) + 1, frogs.size()))
