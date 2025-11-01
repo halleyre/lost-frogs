2277046344303a8334aaf784f82a3d3efa80680d
halleyre <me@halleyre.dev>
2025-11-01 11:50:56 +1100

    Tweak #3 Frog Movement cleaned up frog movement code  - vector based  - sprites aligned with natural rotation

--- godot/frogs/frog.gd
+++ godot/frogs/frog.gd
@@ -1,30 +1,23 @@
 class_name Frog
 extends Sprite2D
 
-enum {UP, RIGHT, DOWN, LEFT}
-# order is important ^^vv
-func dir_to_rot(dir) -> float:
-	return dir * PI/2
+var map: TileMapLayer
+var pos_i: Vector2i
 
-func rot_to_dir(rot) -> int:
-	rot = fposmod(rot, TAU)
-	var dir = UP as int
-	while rot > PI/4:
-		rot -= PI/2
-		dir += 1
-	return dir % 4
+func invalid_move():
+	pass
 	
 func move_back():
-	pass
+	invalid_move()
 
 func _on_move_just_pressed(dir):
-	if posmod(dir - rot_to_dir(rotation), 4) == 2:
+	if roundi(Level.DIR_VECTORS[dir].dot(Vector2.from_angle(rotation))) == -1:
 		move_back()
 		return
-	rotation = dir_to_rot(dir)
+
+	rotation = Level.DIR_VECTORS[dir].angle()
+	position += Level.DIR_VECTORS[dir] * 32
 	
-	match dir:
-		UP:		position.y -= 32
-		RIGHT:	position.x += 32
-		DOWN:	position.y += 32
-		LEFT:	position.x -= 32
+	
+	
+	
--- godot/levels/level.gd
+++ godot/levels/level.gd
@@ -1,39 +1,44 @@
 class_name Level
 extends TileMapLayer
 
+enum {UP, RIGHT, DOWN, LEFT}
+const DIR_VECTORS = {
+	UP:    Vector2.UP,
+	RIGHT: Vector2.RIGHT,
+	DOWN:  Vector2.DOWN,
+	LEFT:  Vector2.LEFT}
+
 var controls: LevelControls = preload("res://ui/level_controls.tscn").instantiate()
 
 var frogs
 var active_frog
 var camera
-var active_pan = {
-	Frog.UP:    false,
-	Frog.RIGHT: false,
-	Frog.DOWN:  false,
-	Frog.LEFT:  false}
-const pan_vectors = {
-	Frog.UP:    Vector2.UP,
-	Frog.RIGHT: Vector2.RIGHT,
-	Frog.DOWN:  Vector2.DOWN,
-	Frog.LEFT:  Vector2.LEFT}
+var active_pan = {}
 	
 @export var camera_speed = 200
 
 func _ready():
+	# start on frog with camera
+	frogs = get_children().filter(func(node): return node is Frog)
+	active_frog = frogs.find_custom(func(node): return node.has_node("Focus"))
+	
+	# init camera
+	camera = frogs[active_frog].get_node("Focus")
+	for dir in DIR_VECTORS:
+		active_pan[dir] = false
+	
+	# connect frog signals
+	controls.move_just_pressed.connect(frogs[active_frog]._on_move_just_pressed)
+	
+	# connect level signals
 	controls.pan_just_pressed.connect(_on_pan_just_pressed)
 	controls.pan_released.connect(_on_pan_released)
 	controls.focus.connect(_on_focus)
-	
-	frogs = get_children().filter(func(node): return node is Frog)
-	active_frog = frogs.find_custom(func(node): return node.has_node("Focus"))
-	camera = frogs[active_frog].get_node("Focus")
-	
-	controls.move_just_pressed.connect(frogs[active_frog]._on_move_just_pressed)
 
 func _process(delta):
-	for dir in pan_vectors:
+	for dir in DIR_VECTORS:
 		if active_pan[dir]:
-			camera.position += delta * camera_speed * pan_vectors[dir]
+			camera.position += delta * camera_speed * DIR_VECTORS[dir]
 	
 func _on_pan_just_pressed(dir):
 	camera.reparent(self)
