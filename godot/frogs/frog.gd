2378ebdf8b2806aac367f2b6f32cdc281a50ce49
halleyre <me@halleyre.dev>
2025-11-05 13:15:35 +1100

    WIP: #8 Tile Aware Movement added - conditional shift / walk

--- godot/frogs/frog.gd
+++ godot/frogs/frog.gd
@@ -1,21 +1,41 @@
 class_name Frog
 extends Area2D
 
-func invalid_move():
-	pass
+# NB: keep this updated or dynamically set it
+const TILE_SIZE = 32
+
+var dead = false
+var height = 0
+
+var no_shift
+var no_walk
+func _ready():
+	no_shift = get_node("NoShift")
+	no_walk = get_node("NoWalk")
+
+func raycast(dir_v, ray) -> bool:
+	ray.global_rotation = dir_v.angle()
+	ray.force_raycast_update()
+	return ray.is_colliding()
+
+# SHIFT: passive move
+func shift(dir_v) -> bool:
+	if raycast(dir_v, no_shift): return false
 	
-func move_back():
-	invalid_move()
+	
+	position += dir_v * TILE_SIZE
+	return true
+	
+# WALK: active move
+func walk(dir_v) -> bool:
+	if roundi(dir_v.dot(Vector2.from_angle(rotation))) == -1: return false
+	if raycast(dir_v, no_walk): return false
+	if !shift(dir_v): return false
+	rotation = dir_v.angle()
+	return true
 
 func _on_move_just_pressed(dir):
+	if dead: return
+	
 	var dir_v = Level.DIR_VECTORS[dir]
-	if roundi(dir_v.dot(Vector2.from_angle(rotation))) == -1:
-		move_back()
-		return
-
-	rotation = dir_v.angle()
-	position = transform * get_node("RayCast2D").target_position
-	
-	
-	
-	
+	walk(dir_v)
