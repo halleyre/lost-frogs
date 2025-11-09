db4a93a22bc5c35efe5404b89dabfa9dadc5b8b1
halleyre <me@halleyre.dev>
2025-11-10 09:49:37 +1100

    Issue #9 Level Control Adv - Frog Interaction frogs push other frogs

--- godot/frogs/frog.gd
+++ godot/frogs/frog.gd
@@ -13,24 +13,42 @@ func _ready():
 	no_shift = get_node("NoShift")
 	no_walk = get_node("NoWalk")
 
-func raycast(dir_v, ray) -> bool:
+func raycast(dir_v, ray) -> Object:
 	ray.global_rotation = dir_v.angle()
 	ray.force_raycast_update()
-	return ray.is_colliding()
+	return ray.get_collider()
 
 # SHIFT: passive move
 func shift(dir_v) -> bool:
-	if raycast(dir_v, no_shift): return false
-	position += dir_v * tile_size
-	resolve_move()
-	return true
+	var obstruction = raycast(dir_v, no_shift)
+	if !resolve_shift(obstruction, dir_v): return false
 	
+	position += dir_v * tile_size
+	var tile = map.get_cell_tile_data(map.local_to_map(position))
+	no_shift.collision_mask &= ~Level.HEIGHT_MASK
+	no_shift.set_collision_mask_value(tile.get_custom_data("exit_height"), true)
+	
+	return true
+
 # WALK: active move
 func walk(dir_v) -> bool:
 	if roundi(dir_v.dot(Vector2.from_angle(rotation))) == -1: return false
-	if raycast(dir_v, no_walk): return false
+	var obstruction = raycast(dir_v, no_walk)
+	if !resolve_walk(obstruction, dir_v): return false
 	if !shift(dir_v): return false
 	rotation = dir_v.angle()
+	return true
+
+func resolve_shift(obstruction, dir_v) -> bool:
+	if obstruction:
+		if obstruction is Frog:
+			if !obstruction.shift(dir_v): return false
+		else: return false
+	return true
+
+func resolve_walk(obstruction, _dir_v) -> bool:
+	if obstruction:
+		return false
 	return true
 
 func _on_move_just_pressed(dir):
@@ -38,8 +56,3 @@ func _on_move_just_pressed(dir):
 	
 	var dir_v = Level.DIR_VECTORS[dir]
 	walk(dir_v)
-	
-func resolve_move():
-	var tile = map.get_cell_tile_data(map.local_to_map(position))
-	no_shift.collision_mask &= ~Level.HEIGHT_MASK
-	no_shift.set_collision_mask_value(tile.get_custom_data("exit_height"), true)
